d9f3ae5b2d11d8376f8cf434985283d3324031fa
Jere Vaara <jere.vaara@aalto.fi>
2018-11-09 00:58:52 +0200

    remove undeeded code

--- src/views/Settings/index.js
+++ src/views/Settings/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native';
 import { withFirebase } from 'react-redux-firebase';
-import { compose } from 'recompose';
 
 import ProfileSettings from './ProfileSettings';
 import ThemeSettings from './ThemeSettings';
 
-const styles = StyleSheet.create({
-  container: {
-  },
-});
-
-const SettingsView = ({
-  firebase,
-  navigation,
-}) => (
-  <ScrollView styles={styles.container}>
+const SettingsView = () => (
+  <ScrollView>
     <ProfileSettings />
     <ThemeSettings />
   </ScrollView>
