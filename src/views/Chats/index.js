21625fa8eb4defd7a5284fcfbd2a71c2a8f845b8
Jere Vaara <jere.vaara@aalto.fi>
2018-11-12 15:32:05 +0200

    fix chat list

--- src/views/Chats/index.js
+++ src/views/Chats/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Text, ScrollView, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-import { isLoaded, withFirebase, populate } from 'react-redux-firebase';
-import { withTheme } from '../../components/ThemedWrapper';
+import { isLoaded, withFirebase } from 'react-redux-firebase';
 
+import { withTheme } from '../../components/ThemedWrapper';
 import ChatList from './ChatList';
 
 const styles = StyleSheet.create({
@@ -13,14 +13,15 @@ const styles = StyleSheet.create({
   },
 });
 
-const ChatMock = ({ chats, useDarkTheme }) => {
-  console.log(`chats: ${JSON.stringify(chats, null, 2)}`);
-  return (
-    <ScrollView style={useDarkTheme && styles.dark}>
-      {!isLoaded(isLoaded || chats) ? <Text>Loading</Text> : <ChatList chats={chats} useDarkTheme={useDarkTheme} />}
-    </ScrollView>
-  );
-};
+const Chats = ({ chats, useDarkTheme }) => (
+  <ScrollView style={useDarkTheme && styles.dark}>
+    {!isLoaded(chats) ? (
+      <Text>Loading</Text>
+    ) : (
+      <ChatList chats={chats} useDarkTheme={useDarkTheme} />
+    )}
+  </ScrollView>
+);
 
 const mapStateToProps = state => ({
   chats: state.firebase.profile.chats,
@@ -30,4 +31,4 @@ export default compose(
   withTheme,
   withFirebase,
   connect(mapStateToProps),
-)(ChatMock);
+)(Chats);
