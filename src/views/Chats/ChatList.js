1e62da9652e3b65a87810fbe8c046462f7393d3e
Jere Vaara <jere.vaara@aalto.fi>
2018-11-12 15:31:54 +0200

    use lodash map to render chats

--- src/views/Chats/ChatList.js
+++ src/views/Chats/ChatList.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import _map from 'lodash/map';
 
 import ChatPreview from '../../components/ChatPreview';
 
 const ChatList = ({
   chats,
-  useDarkTheme,
-}) => {
-  console.log(`useDarkTheme: ${useDarkTheme}`);
-  if (chats) {
-    console.log(`chats ${JSON.stringify(chats)}`);
-    const chatList = Object.values(Object.values(chats));
-    console.log(`chatList ${JSON.stringify(chatList, null, 2)}`);
-    const namedChats = Object.keys(chatList).map(key => ({ id: key, chat: chats[key] }));
-    console.log(`namedChats ${JSON.stringify(namedChats, null, 2)}`);
-    return (
-      <View>
-        { namedChats
-          ? namedChats.map(chat => <ChatPreview key={chat.id} chat={chat.chat} useDarkTheme={useDarkTheme} />)
-          : <Text>Loading chats...</Text>
-        }
-      </View>
-    );
-  }
-  return <Text>Loading...</Text>;
-};
+}) => (
+  <View>
+    { _map(chats, (chat, key) => <ChatPreview key={key} chat={chat} />) }
+  </View>
+);
 
 export default ChatList;
