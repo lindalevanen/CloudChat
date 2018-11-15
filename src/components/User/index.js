853973da71c57fd93dcbd8b82b3af53a04086f50
Jere Vaara <jere.vaara@aalto.fi>
2018-11-15 17:44:11 +0200

    pass username to avatar component to use as placeholder

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -28,7 +28,7 @@ const User = ({
   const style = styles(theme);
   return (
     <View style={style.container}>
-      <Avatar url={user.avatarUrl} />
+      <Avatar url={user.avatarUrl} username={user.username} />
       <Text style={style.text}>{user.username}</Text>
     </View>
   );
