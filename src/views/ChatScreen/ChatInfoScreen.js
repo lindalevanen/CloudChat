d07c65035a53e78038ff13e25ff2a4d201ee7635
Jere Vaara <jere.vaara@aalto.fi>
2018-11-16 14:12:43 +0200

    chat info screen mvp

--- src/views/ChatScreen/ChatInfoScreen.js
+++ src/views/ChatScreen/ChatInfoScreen.js
@@ -3,26 +3,79 @@ import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import { compose } from 'recompose';
 import { firebaseConnect, populate } from 'react-redux-firebase';
+import _map from 'lodash/map';
 
-const ChatInfoScreen = ({
-  chat,
-  chatMembers,
-}) => (
-  <View>
-    <Text>ChatInfoScreen</Text>
-    <Text>{JSON.stringify(chat, '', 2)}</Text>
-  </View>
-);
+import { withTheme } from '../../components/ThemedWrapper';
+import Avatar from '../../components/Avatar';
+import Button from '../../components/Button';
+import { styles } from '../../styles/form/style';
+import UserList from '../../components/UserList';
 
-const populates = [
-  { child: 'members', root: 'users' },
-];
+const infoStyles = theme => ({
+  container: {
+    backgroundColor: theme.backdrop,
+    flex: 1,
+  },
+  section: {
+    backgroundColor: theme.foreground,
+    borderColor: theme.separator,
+    borderBottomWidth: 0.3,
+  },
+  topContainer: {
+    padding: 10,
+    flexDirection: 'row',
+  },
+  profileText: {
+    marginLeft: 10,
+    fontSize: 16,
+    color: theme.text1,
+  },
+});
+
+const ChatInfoScreen = ({ theme, chat }) => {
+  const style = styles(theme);
+  const infoStyle = infoStyles(theme);
+  return (
+    <View style={style.view}>
+      <View style={[infoStyle.topContainer, style.section]}>
+        <Avatar url={chat.avatarUrl} username={chat.title} />
+        <View>
+          <Text
+            style={[
+              infoStyle.profileText,
+              { marginTop: 10, marginBottom: 5, fontWeight: 'bold' },
+            ]}
+          >
+            {chat.title}
+          </Text>
+          <Text style={[infoStyle.profileText, { marginBottom: 10 }]}>
+            {`${Object.keys(chat.members).length} members`}
+          </Text>
+        </View>
+      </View>
+      <View style={style.section}>
+        <UserList users={_map(chat.members, (user, id) => ({ id, ...user }))} />
+      </View>
+      <View style={infoStyle.section}>
+        <Button
+          title="Leave group"
+          titleStyle={{ alignSelf: 'flex-start' }}
+          color="transparent"
+          titleColor={theme.actionHero}
+        />
+      </View>
+    </View>
+  );
+};
+
+const populates = [{ child: 'members', root: 'users' }];
 
 const mapStateToProps = ({ firebase }, { navigation }) => ({
   chat: populate(firebase, `chats/${navigation.state.params.chatId}`, populates),
 });
 
 const enhance = compose(
+  withTheme,
   firebaseConnect(({ navigation }) => [
     { path: `chats/${navigation.state.params.chatId}`, populates },
   ]),
