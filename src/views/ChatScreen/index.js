dbc3adaeadd6450f1346c4cd74702f6abc213d15
haariot1 <timo.haario@aalto.fi>
2018-11-16 14:37:05 +0200

    working message sending to group chat

--- src/views/ChatScreen/index.js
+++ src/views/ChatScreen/index.js
@@ -1,22 +1,51 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
-import { compose } from 'recompose';
+import { compose, withState } from 'recompose';
 import { firebaseConnect, populate } from 'react-redux-firebase';
+import TextInput from '../../components/TextInput';
+import Button from '../../components/Button';
 
-const ChatScreen = ({ chat }) => (
+const ChatScreen = ({
+  chat,
+  messageString,
+  setMessageString,
+  firebase,
+  navigation,
+}) => (
   <View>
-    <Text>{JSON.stringify(chat.messages ?? 'no messages', '', 2)}</Text>
+
+    <TextInput
+      placeholder="Message"
+      onChangeText={setMessageString}
+      autoCorrect={false}
+      autoCapitalize="none"
+      value={messageString}
+    />
+    <Button title="Send" onPress={() => sendMessage(firebase, messageString, navigation.state.params.chatId)} />
   </View>
 );
 
-const populates = [{ child: 'members', root: 'users' }];
+const sendMessage = async (firebaseRef, messageString, chatId) => {
+  const messageData = {
+    body: messageString,
+    createdAt: Date.now(),
+    attachment: '',
+  };
+  const res = firebaseRef.push(`chats/${chatId}/messages`, messageData);
+  console.log(res);
+};
+
+const populates = [
+  { child: 'members', root: 'users' },
+];
 
 const mapStateToProps = ({ firebase }, { navigation }) => ({
   chat: populate(firebase, `chats/${navigation.state.params.chatId}`, populates),
 });
 
 const enhance = compose(
+  withState('messageString', 'setMessageString', ''),
   firebaseConnect(({ navigation }) => [
     { path: `chats/${navigation.state.params.chatId}`, populates },
   ]),
