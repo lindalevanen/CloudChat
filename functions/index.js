418ab5f08948fed8d3f239c9a622d001761cd9b7
Laged <parkkila.matti@gmail.com>
2018-11-19 18:53:48 +0200

    Fix broken createAvatarThumbnail -function

--- functions/index.js
+++ functions/index.js
@@ -14,23 +14,45 @@ function getFileName(key, tempName) {
   return `${key}_${tempName}`
 }
 
-function resizePromise(configuration, tempFileName, tempFileName) {
+function resizePromise(configuration, tempFilePath, tempFileName) {
   const { key, resolution } = configuration;
   console.log(`resizePromise: ${key} / ${tempFileName}`);
-  const fileName = getFileName(key, tempFileName);
-+  console.log(`fileName ${fileName}`);
-  return spawn('convert', [tempFileName, '-thumbnail', resolution, fileName])
+  const origFile = `${tempFilePath}/${tempFileName}`;
+  const convertedFile = `${tempFilePath}/${getFileName(key, tempFileName)}`;
+  console.log(`origFile ${origFile}`);
+  console.log(`convertedFile ${convertedFile}`);
+  return spawn('convert', [origFile, '-thumbnail', resolution, convertedFile])
 }
 
-function uploadPromise(configuration, tempFilePath, tempFileName, metadata) {
+function uploadPromise(bucket, configuration, tempFilePath, tempFileName, uploadDir, metadata) {
   const { key, resolution } = configuration;
+  console.log(`tempFileName ${tempFileName}`);
   console.log(`uploadPromise: ${key}`);
   const fileName = getFileName(key, tempFileName);
   console.log(`fileName ${fileName}`);
+  const convertedFile = `${tempFilePath}/${getFileName(key, tempFileName)}`;
+  console.log(`convertedFile ${convertedFile}`);
+  const fileDest = `${uploadDir}/${fileName}`;
+  console.log(`fileDest ${fileDest}`);
   // Uploading the thumbnail.
-  return bucket.upload(tempFilePath, {
-    destination: uploadFilePath,
+  return bucket.upload(convertedFile, {
+    destination: fileDest,
     metadata,
+  });
+}
+
+function deleteFiles(tempFilePath) {
+  console.log(`deleteFiles ${tempFilePath}`);
+  return new Promise( (resolve, reject) => {
+    files = glob.glob(`*${tempFilePath}`, (err, files) => {
+      if (err) {
+        console.err(err);
+        reject(err);
+      }
+      console.log(`Deleting files ${files}`);
+      files.forEach(fs.unlinkSync);
+      return(files)
+    });
   });
 }
 
@@ -52,23 +74,9 @@ exports.createAvatarThumbnail = functions.region(region).storage.object().onFina
   // Get the file name
   console.log(`filePath ${filePath}`);
   const fileName = path.basename(filePath);
+  const fileDir = path.dirname(filePath);
   console.log(`fileName ${fileName}`);
-  // Exit if the image is already a thumbnail.
-  if (fileName.startsWith('thumb_')) {
-    return console.log('Already a Thumbnail.');
-  }
-
-  // Download file from bucket.
-  const bucket = admin.storage().bucket(fileBucket);
-  const tempPath = os.tmpdir()
-  const tempFilePath = path.join(tempPath, fileName);
-  console.log(`tempFilePath ${tempFilePath}`);
-  const metadata = {
-    contentType,
-  };
-  await bucket.file(filePath).download({ destination: tempFilePath });
-  console.log('Image downloaded locally to', tempFilePath);
-  // Generate a thumbnail using ImageMagick to resolutions thumb (200x200 px), low (640×480 px), high (1280×960 px)
+  // Exit if the image is already resized.
   const resolutions = [
     {
       key: 'thumb',
@@ -83,26 +91,36 @@ exports.createAvatarThumbnail = functions.region(region).storage.object().onFina
       resolution: '1280x960>'
     }
   ]
-  // WIP: make sure these work for all files&resolutions
+  const isResized = resolutions.some(i => i.key === fileName.slice(0, i.key.length));
+  console.log(`isResized ${isResized}`);
+  if (isResized) {
+    return console.log('Already a Thumbnail.');
+  }
+
+  // Download file from bucket.
+  const bucket = admin.storage().bucket(fileBucket);
+  const tempPath = os.tmpdir()
+  const tempFilePath = path.join(tempPath, fileName);
+  console.log(`tempFilePath ${tempFilePath}`);
+  const metadata = {
+    contentType,
+  };
+  await bucket.file(filePath).download({ destination: tempFilePath });
+  console.log('Image downloaded locally to', tempFilePath);
+  // Generate a thumbnail using ImageMagick to resolutions thumb (200x200 px), low (640×480 px), high (1280×960 px)
   // Resize images
-  const resizePromises = resolutions.map(i => resizePromise(i, tempFilePath, fileName));
-  const converted = Promise.all(resizePromises);
+  const resizePromises = resolutions.map(i => resizePromise(i, tempPath, fileName));
+  const converted = await Promise.all(resizePromises);
   console.log(`${resolutions.length} thumbnails created at ${tempFilePath}`);
   // Upload images
-  const uploadPromises = resolutions.map(i => uploadPromise(i, tempPath, fileName, metadata));
-  const uploaded = Promise.all(uploadPromises);
+  const uploadPromises = resolutions.map(i => uploadPromise(bucket, i, tempPath, fileName, fileDir, metadata));
+  console.log('Uploading files...')
+  const uploaded = await Promise.all(uploadPromises);
+  console.log('Uploaded files');
   console.log(`${resolutions.length} images uploaded`);
   // Once the thumbnail has been uploaded delete the local file to free up disk space.
   console.log('Deleting files...');
-  files = glob.glob(`*${tempFilePath}`, (err, files) => {
-    if (err) {
-      console.err(err);
-      return err
-    }
-    console.log(`Deleting files ${files}`);
-    files.forEach(fs.unlinkSync);
-    return files
-  });
+  const files = await deleteFiles(tempFilePath)
   console.log(`Deleted files ${files}`);
   return files
 });
