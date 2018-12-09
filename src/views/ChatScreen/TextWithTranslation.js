f046bbd53132cc3e02212fd1377d02819114c1e3
Jere Vaara <jere.vaara@aalto.fi>
2018-12-09 13:22:55 +0200

    Modal to view all translations of a message

--- src/views/ChatScreen/TextWithTranslation.js
+++ src/views/ChatScreen/TextWithTranslation.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {
+  Text, View, TouchableOpacity, Modal,
+} from 'react-native';
 import { connect } from 'react-redux';
 import { Ionicons } from '@expo/vector-icons';
 import { Circle } from 'react-native-progress';
+import { withState, compose } from 'recompose';
+import _map from 'lodash/map';
 
 import { options as languages } from '../Settings/LanguageSetting';
 
@@ -16,22 +20,106 @@ const themedStyles = theme => ({
     padding: 2,
     paddingHorizontal: 6,
   },
+  modal: {
+    backgroundColor: 'red',
+    opacity: 0.4,
+  },
+  modalHeader: {
+    marginBottom: 6,
+  },
+  modalTitle: {
+    fontSize: 20,
+    color: theme.text1,
+  },
+  modalBackdrop: {
+    flex: 1,
+  },
+  modalContent: {
+    position: 'absolute',
+    marginTop: 200,
+    left: 0,
+    right: 0,
+    marginHorizontal: 20,
+    backgroundColor: theme.foreground,
+    padding: 10,
+    minHeight: 200,
+    borderRadius: 10,
+  },
+  translationItem: {
+    marginTop: 4,
+    padding: 6,
+    paddingHorizontal: 8,
+    backgroundColor: theme.messageBubble,
+    borderRadius: 8,
+    borderTopLeftRadius: 2,
+    alignSelf: 'flex-start',
+  },
+  text: {
+    color: theme.text2,
+  },
 });
 
+const TranslationViewerModal = ({
+  theme, styles, original, translations, close, ...props
+}) => (
+  <Modal style={styles.modal} {...props} onRequestClose={close} animationType="slide" transparent>
+    <TouchableOpacity style={styles.modalBackdrop} onPress={close} />
+    <View style={styles.modalContent}>
+      <View style={styles.modalHeader}>
+        <Text style={styles.modalTitle}>Translations</Text>
+      </View>
+      <View style={{ backgroundColor: theme.ownMessage, marginBottom: 6 }}>
+        <Text style={[styles.text]}>Original message:</Text>
+        <Text style={styles.text}>{original}</Text>
+      </View>
+      {_map(translations, (value, key) => (
+        <View key={key} style={styles.translationItem}>
+          <Text style={[styles.text, { fontStyle: 'italic' }]}>{`${languages.find(val => val.key === key).label}:`}</Text>
+          <Text style={styles.text}>{value}</Text>
+        </View>
+      ))}
+    </View>
+  </Modal>
+);
+
 const TextWithTranslation = ({
-  theme, style, language, loading, body,
+  theme,
+  style,
+  language,
+  payload,
+  loading,
+  body,
+  modalOpen,
+  setModalOpen,
 }) => {
   const styles = themedStyles(theme);
   return (
     <View>
       <Text style={style}>{body}</Text>
       {language ? (
-        <View style={styles.footer}>
-          {!loading ? <Ionicons name="ios-globe" size={12} color="white" />
-            : <Circle indeterminate size={12} color="white" />
-        }
-          <Text style={[style, { fontSize: 11, marginLeft: 4 }]}>{language}</Text>
-        </View>
+        <>
+          <TouchableOpacity
+            style={styles.footer}
+            onPress={() => setModalOpen(true)}
+          >
+            {!loading ? (
+              <Ionicons name="ios-globe" size={12} color="white" />
+            ) : (
+              <Circle indeterminate size={12} color="white" />
+            )}
+            <Text style={[style, { fontSize: 11, marginLeft: 4 }]}>
+              {language}
+            </Text>
+          </TouchableOpacity>
+          <TranslationViewerModal
+            theme={theme}
+            styles={styles}
+            original={payload.body}
+            translations={payload.translations}
+            visible={modalOpen}
+            close={() => setModalOpen(false)}
+          />
+        </>
       ) : null}
     </View>
   );
@@ -60,4 +148,9 @@ const mapStateToProps = (state, props) => ({
   body: constructMessageBody(state.settings.messageLanguage, props.payload),
 });
 
-export default connect(mapStateToProps)(TextWithTranslation);
+const enhance = compose(
+  withState('modalOpen', 'setModalOpen', false),
+  connect(mapStateToProps),
+);
+
+export default enhance(TextWithTranslation);
